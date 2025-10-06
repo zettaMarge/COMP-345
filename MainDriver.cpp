9852443921b8b44754d1f98a8fc35900886d0e7e
kallestrous1 <106293642+kallestrous1@users.noreply.github.com>
2025-10-06 12:22:42 -0400

    added all testmethod calls to maindriver

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -1,5 +1,20 @@
 #include "GameEngine.h"
+#include "Map/MapDriver.cpp"
+#include "OrdersDriver.cpp"
+#include "PlayerDriver.cpp"
+#include "CardsDriver.cpp"
+
 int main() {
+	TestLoadMaps({
+		"../Maps/_11_ Against All Odds.map",
+		"../Maps/001_I72_Ghtroc 720.map",
+		"../Maps/005_I72_V-22.map",
+		"../Maps/READ FIRST.txt",
+		"../Maps/_64_ BIG BLUE.map"
+		});
+	testPlayers();
+	testOrdersLists();
     TestGameEngine();
+	return 0;
 }
 
