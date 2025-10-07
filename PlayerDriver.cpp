cf817b6f40789e8eaa554db0b4843a8e0f6e6000
MiaLetendre <mialetendre98@gmail.com>
2025-10-07 14:58:21 -0400

    Changed the toAttavck Function in players.cpp and fixed player.cpp

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -15,7 +15,6 @@
 #include <iostream>
 using namespace std;
 
-//need to run with g++ -mconsole PlayerDriver.cpp Players.cpp Map.cpp Orders.cpp Cards.cpp -o testPlayers.exe
 
 
 int testPlayers() {
@@ -29,6 +28,9 @@ int testPlayers() {
     testMap->AddTerritory(t2->GetName(), cont1);
     testMap->AddTerritory(t3->GetName(), cont1);
     Player p1;
+    testMap->AddAdjacency(t1, t2);
+    testMap->AddAdjacency(t2, t3);
+    testMap->AddAdjacency(t3, t1);
     p1.SetName("Player1");
     p1.AddTerritory(t1);
     p1.AddTerritory(t2);
@@ -50,6 +52,9 @@ int testPlayers() {
     cout << endl;
     vector<Territory*> attackList = p1.ToAttack(); // why doesnt this work
     cout << "Territories to attack: ";
+    if (attackList.size() == 0) {
+        cout << "None";
+    }
     for (Territory* t : attackList) {
         cout << t->GetName() << " ";
     }
@@ -59,18 +64,19 @@ int testPlayers() {
     p1.IssueOrder(&order1);
     Advance order2(&p1, 4, t1, t2);
     p1.IssueOrder(&order2);
-    cout << "Orders issued by " << p1.GetName() << ": ";
-    cout << p1.GetPlayerOrders() << endl;
-    cout << endl;
-
+    cout << "Orders issued by " << p1.GetName() ;// << ":\n" *p1.GetPlayerOrders() << endl;
+    for (Order* o : p1.GetPlayerOrders()->GetListItems()) {
+        cout << *o;
+    }
+    
 	Deck* deck1 = new Deck();
     Hand* hand1 = new Hand(*deck1);
     hand1->AddCard();
     hand1->AddCard();
     p1.SetPlayerHand(hand1);
-    cout << "Hand of Player1: ";
-    cout<< p1.GetPlayerHand() << endl;
-    cout << endl;
+    
+    cout << "" << p1.GetName() << " "; //<< ":\n" << *p1.GetPlayerHand() << endl;
+    p1.GetPlayerHand()->Print();
     return 0;
 }
 
