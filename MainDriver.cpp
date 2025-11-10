754a0daede48aa7f031a10876f2948788ac7e4a9
SVA-BL00 <sarav.notes@gmail.com>
2025-11-10 16:49:19 -0500

    Added logging observer driver test to main

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -1,12 +1,15 @@
 #include "GameEngine.h"
 #include "CommandProcessing.h"
 
+void testLoggingObserver();
+
 int main() {
 	GameEngine* game = new GameEngine();
 	GameEngine::instance = game;
 	game->TestStartupPhase();
 	game->TestOrderExecution();
 	TestGameEngine();
+	// testLoggingObserver();
 	//command procceing i think is done in the command processing driver	
 
 	//Assignment 1 tests
